3f8ce8139e497d05e872ec13a315ce6879090d6f
Mathias Wuerthle <mathias.wuerthle@gmx.de>
2016-09-26 10:25:08 +0200

    Remove duplicate methods

--- src/android/nl/xservices/plugins/Calendar.java
+++ src/android/nl/xservices/plugins/Calendar.java
@@ -479,41 +479,6 @@ public class Calendar extends CordovaPlugin {
       return;
     }
 
-    // note that if the dev didn't call requestWritePermission before calling this method and calendarPermissionGranted returns false,
-    // the app will ask permission and this method needs to be invoked again (done for backward compat).
-    if (!calendarPermissionGranted(Manifest.permission.WRITE_CALENDAR)) {
-      requestWritePermission();
-      this.callback.error("Please allow Write access to the Calendar and try again.");
-      return;
-    }
-
-    try {
-      final JSONObject jsonFilter = args.getJSONObject(0);
-
-      cordova.getThreadPool().execute(new Runnable() {
-        @Override
-        public void run() {
-
-          boolean deleteResult = getCalendarAccessor().deleteEventWithId(
-              jsonFilter.optInt("id"),
-              null);
-          PluginResult res = new PluginResult(PluginResult.Status.OK, deleteResult);
-          res.setKeepCallback(true);
-          callback.sendPluginResult(res);
-        }
-      });
-    } catch (JSONException e) {
-      System.err.println("Exception: " + e.getMessage());
-      callback.error(e.getMessage());
-    }
-  }
-
-  private void deleteEventWithId(JSONArray args) {
-    if (args.length() == 0) {
-      System.err.println("Exception: No Arguments passed");
-      return;
-    }
-
     if (!calendarPermissionGranted(Manifest.permission.WRITE_CALENDAR)) {
       requestWritePermission(PERMISSION_REQCODE_DELETE_EVENT_WITH_ID);
       return;
@@ -676,92 +641,6 @@ public class Calendar extends CordovaPlugin {
             }
           } catch (JSONException e) {
             e.printStackTrace();
-          }
-        }
-      });
-    } catch (Exception e) {
-      Log.e(LOG_TAG, "Error creating event. Invoking error callback.", e);
-      callback.error(e.getMessage());
-    }
-  }
-
-  private void modifyEventWithId(JSONArray args) {
-    if (!calendarPermissionGranted(Manifest.permission.WRITE_CALENDAR)) {
-      requestWritePermission(PERMISSION_REQCODE_MODIFY_EVENT_WITH_ID);
-      return;
-    }
-
-    try {
-      final JSONObject argObject = args.getJSONObject(0);
-      final JSONObject argOptionsObject = argObject.getJSONObject("options");
-
-      cordova.getThreadPool().execute(new Runnable() {
-        @Override
-        public void run() {
-          try {
-            final boolean modifyResult = getCalendarAccessor().modifyEventWithId(
-                argObject.getInt("id"),
-                null,
-                getPossibleNullString("title", argObject),
-                argObject.getLong("startTime"),
-                argObject.getLong("endTime"),
-                getPossibleNullString("notes", argObject),
-                getPossibleNullString("location", argObject),
-                getPossibleNullString("recurrence", argOptionsObject),
-                argOptionsObject.optInt("recurrenceInterval"),
-                argOptionsObject.optLong("recurrenceEndTime"),
-                argOptionsObject.optInt("calendarId", 1),
-                getPossibleNullString("url", argOptionsObject));
-
-            PluginResult res = new PluginResult(PluginResult.Status.OK, modifyResult);
-            res.setKeepCallback(true);
-            callback.sendPluginResult(res);
-          } catch (JSONException e) {
-            Log.e(LOG_TAG, "Error creating event. Invoking error callback.", e);
-            callback.error(e.getMessage());
-          }
-        }
-      });
-    } catch (Exception e) {
-      Log.e(LOG_TAG, "Error creating event. Invoking error callback.", e);
-      callback.error(e.getMessage());
-    }
-  }
-
-  private void modifyEventWithId(JSONArray args) {
-    if (!calendarPermissionGranted(Manifest.permission.WRITE_CALENDAR)) {
-      requestWritePermission(PERMISSION_REQCODE_MODIFY_EVENT_WITH_ID);
-      return;
-    }
-
-    try {
-      final JSONObject argObject = args.getJSONObject(0);
-      final JSONObject argOptionsObject = argObject.getJSONObject("options");
-
-      cordova.getThreadPool().execute(new Runnable() {
-        @Override
-        public void run() {
-          try {
-            final boolean modifyResult = getCalendarAccessor().modifyEventWithId(
-                argObject.getInt("id"),
-                null,
-                getPossibleNullString("title", argObject),
-                argObject.getLong("startTime"),
-                argObject.getLong("endTime"),
-                getPossibleNullString("notes", argObject),
-                getPossibleNullString("location", argObject),
-                getPossibleNullString("recurrence", argOptionsObject),
-                argOptionsObject.optInt("recurrenceInterval"),
-                argOptionsObject.optLong("recurrenceEndTime"),
-                argOptionsObject.optInt("calendarId", 1),
-                getPossibleNullString("url", argOptionsObject));
-
-            PluginResult res = new PluginResult(PluginResult.Status.OK, modifyResult);
-            res.setKeepCallback(true);
-            callback.sendPluginResult(res);
-          } catch (JSONException e) {
-            Log.e(LOG_TAG, "Error creating event. Invoking error callback.", e);
-            callback.error(e.getMessage());
           }
         }
       });
